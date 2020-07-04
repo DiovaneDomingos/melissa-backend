01bb794c29fc0178f2709d331978877cd1bc8a8c
danitw <twz3xc21@gmail.com>
2020-07-04 12:18:05 -0300

    retirando a rota tracking q eu esqueci

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,5 @@ const routes = express.Router()
 
 routes.post('/api/client', MessageController.client)
 routes.post('/api/store',  MessageController.store)
-routes.post('/api/tracking',  MessageController.tracking)
 
 module.exports = routes
